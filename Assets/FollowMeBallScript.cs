a5471e72552073e06e5cf910fea4894ebb0b3420
AndStyver <andsty0125@skola.goteborg.se>
2022-09-16 10:18:09 +0200

    Added comments to the code

--- Assets/FollowMeBallScript.cs
+++ Assets/FollowMeBallScript.cs
@@ -30,36 +30,38 @@ public class FollowMeBallScript : ProcessingLite.GP21
         Background(0); //Clears the background and sets the color to 0.
         StrokeWeight(1);
         Stroke(255);
-        Circle(x, y, diameter);
 
+        //add movement to the ball
         randomX = randomX + (xSpeed / 10);
         randomY = randomY + (ySpeed / 10);
 
+        //make the ball bounce on the edges of the screen
         if (randomX > Width || randomX <= 0) { xSpeed *= -1; }
         if (randomY > Height || randomY <= 0) { ySpeed *= -1; }
 
-
+        //creates the ball and moves it
         Circle(randomX, randomY, 0.2f);
 
         mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
 
+        //Circle that follows the mouse when clicked
+        Circle(x, y, diameter);
+
+        //Updates the circles position when the left mouse button is held
         if (Input.GetMouseButton(0))
         {
-            //Circle(CircleToMouseVector().x, CircleToMouseVector().y, 0.4f);
-
             x += CircleToMouseVector().x;
             y += CircleToMouseVector().y;
         }
 
+        //Draws a line between the circle and the mouse position
         Line(mousePos.x, mousePos.y, x, y);
     }
 
+    //Gets the vector between the circle and the mouse
     Vector2 CircleToMouseVector()
     {
         Vector2 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y));
-
-        //float deltaX = x - mousePos.x;
-        //float deltaY = y - mousePos.y;
 
         Vector2 _vector = mousePos - new Vector2(x, y);
 
