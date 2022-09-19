f8854b74630c50b6465907d054549aa79a4176e1
AndStyver <andsty0125@skola.goteborg.se>
2022-09-19 09:46:09 +0200

    movement with vector done
    you can move with arrowkeys, press g to enable/disable gravity

--- Assets/InputTheBall.cs
+++ Assets/InputTheBall.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class InputTheBall : ProcessingLite.GP21
 {
     float posX;
+    float posY;
     public float diameter;
     public float ballSpeed;
+
+    float v = 1;
+    int a = 1;
+
+    public bool gravity;
 
     // Start is called before the first frame update
     void Start()
     {
         posX = Width / 2;
+        posY = Height / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
         Background(0);
+        v += a * Time.deltaTime; //constant acceleration
 
-        posX += Input.GetAxis("Horizontal") * ballSpeed;
-        Circle(posX, Height / 2, diameter);
+        posX += Input.GetAxis("Horizontal") * ballSpeed * Time.deltaTime;
+
+
+        if (gravity) { posY += -v * Time.deltaTime; }
+        else { posY += Input.GetAxis("Vertical") * ballSpeed * Time.deltaTime; }
+
+        if (Input.GetKeyDown(KeyCode.G)) { gravity = !gravity; }
+
+        Circle(posX, posY, diameter);
     }
 }
