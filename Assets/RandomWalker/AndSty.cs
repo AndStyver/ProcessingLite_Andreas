4cecc56275d8126dde79cf4f3da73e5a17613a3e
AndStyver <andsty0125@skola.goteborg.se>
2022-09-30 15:26:40 +0200

    fixed issue where it could walk offscreen
    updates pVector so that the biases are updated correctly

--- Assets/RandomWalker/AndSty.cs
+++ Assets/RandomWalker/AndSty.cs
@@ -8,9 +8,14 @@ class AndSty : IRandomWalker
     //Add your own variables here.
     //Do not use processing variables like width or height
 
+    Vector2 pVector;
+
     int direction = 0;
-    List<float> biases = new List<float>(4);
+    readonly List<float> biases = new List<float>(4);
     float diceRoll;
+
+    int areaWidth;
+    int areaHeight;
 
     public string GetName()
     {
@@ -20,15 +25,19 @@ class AndSty : IRandomWalker
     public Vector2 GetStartPosition(int playAreaWidth, int playAreaHeight)
     {
         for (int i = 0; i < 4; i++) { biases.Add(0.25f); } //sets all biases to be equally likely to start
-        Debug.Log(biases.Count);
-        Debug.Log(biases[0] + " " + biases[1] + " " + biases[2] + " " + biases[3]);
+
+        areaWidth = playAreaWidth;
+        areaHeight = playAreaHeight;
 
         //Select a starting position or use a random one.
-        float x = (int)UnityEngine.Random.Range(0, playAreaWidth);
+        //float x = (int)UnityEngine.Random.Range(0, playAreaWidth);
         float y = (int)UnityEngine.Random.Range(0, playAreaHeight);
+        float x = playAreaWidth;
+        //float y = playAreaHeight;
 
         //a PVector holds floats but make sure its whole numbers that are returned!
-        return new Vector2(x, y);
+        pVector = new Vector2(x, y);
+        return pVector;
     }
 
     public Vector2 Movement()
@@ -41,15 +50,18 @@ class AndSty : IRandomWalker
         switch (WeightedDecision())
         {
             case 0:
+                pVector.x += 1;
                 return new Vector2(1, 0);
             case 1:
+                pVector.x -= 1;
                 return new Vector2(-1, 0);
             case 2:
+                pVector.y += 1;
                 return new Vector2(0, 1);
             default:
+                pVector.y -= 1;
                 return new Vector2(0, -1);
         }
-
     }
 
     int WeightedDecision()
@@ -62,6 +74,7 @@ class AndSty : IRandomWalker
         for (int i = 0; i < biases.Count; i++)
         {
             cumulative += biases[i];
+            //Debug.Log("During step " + i + " cumulative is " + cumulative + " While diceRoll was " + diceRoll);
             if (diceRoll < cumulative)
             {
                 selectedDirection = i;
@@ -69,12 +82,45 @@ class AndSty : IRandomWalker
             }
         }
 
-        //Debug.Log(selectedDirection);
         return selectedDirection;
     }
 
     void UpdateBiases()
     {
+        if (pVector.x >= areaWidth - 1)
+        {
+            biases[0] = 0f;
+            biases[1] = 0.2f;
+            biases[2] = 0.4f;
+            biases[3] = 0.4f;
+            return;
+        }
+        else if (pVector.x <= 1)
+        {
+            biases[0] = 0.2f;
+            biases[1] = 0f;
+            biases[2] = 0.4f;
+            biases[3] = 0.4f;
+            return;
+        }
+
+        if (pVector.y >= areaHeight - 1)
+        {
+            biases[0] = 0.4f;
+            biases[1] = 0.4f;
+            biases[2] = 0f;
+            biases[3] = 0.2f;
+            return;
+        }
+        else if (pVector.y <= 1)
+        {
+            biases[0] = 0.4f;
+            biases[1] = 0.4f;
+            biases[2] = 0.2f;
+            biases[3] = 0f;
+            return;
+        }
+
         switch (WeightedDecision())
         {
             case 0:
@@ -102,7 +148,7 @@ class AndSty : IRandomWalker
                 biases[3] = 0.3f;
                 break;
         }
-        Debug.Log("bias 0: " + biases[0] + ", bias 1: " + biases[1] + ", bias 2: " + biases[2] + ", bias 3: " + biases[3]);
+        //Debug.Log("bias 0: " + biases[0] + ", bias 1: " + biases[1] + ", bias 2: " + biases[2] + ", bias 3: " + biases[3]);
     }
 }
 
